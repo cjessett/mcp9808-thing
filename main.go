a60f4faf683153a369e2397736cb4e6be22ceac9
CJ Jessett <cjessett@gmail.com>
2019-04-13 06:30:31 -0700

    Log to stdout and file

--- main.go
+++ main.go
@@ -1,130 +1,127 @@
 package main
 
 import (
-  "os"
-  "log"
-  "fmt"
-  "encoding/json"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"os"
 
-  "github.com/joho/godotenv"
-  "github.com/kuzemkon/aws-iot-device-sdk-go/device"
-  "periph.io/x/periph/host"
-  "periph.io/x/periph/conn/i2c/i2creg"
-  "periph.io/x/periph/experimental/devices/mcp9808"
+	"github.com/joho/godotenv"
+	"github.com/kuzemkon/aws-iot-device-sdk-go/device"
+	"periph.io/x/periph/conn/i2c/i2creg"
+	"periph.io/x/periph/experimental/devices/mcp9808"
+	"periph.io/x/periph/host"
 )
 
 type Shadow struct {
-  State struct {
-    Reported struct {
-      Temp int `json:"temp"`
-    } `json:"reported"`
-  } `json:"state"`
-  Version int
+	State struct {
+		Reported struct {
+			Temp int `json:"temp"`
+		} `json:"reported"`
+	} `json:"state"`
+	Version int
 }
 
 func initEnv() error {
-  home, err := os.UserHomeDir()
-  if err != nil {
-    return err
-  }
-  err = godotenv.Load(fmt.Sprintf("%s/.env", home))
-  return err
-}
-
-func initLog() {
-  logPath := os.Getenv("LOG_FILE_PATH")
-  f, err := os.OpenFile(logPath, os.O_RDWR | os.O_CREATE | os.O_APPEND, 0666)
-  if err != nil {
-    log.Fatalf("error opening file: %v", err)
-  }
-  defer f.Close()
-
-  log.SetOutput(f)
+	home, err := os.UserHomeDir()
+	if err != nil {
+		return err
+	}
+	err = godotenv.Load(fmt.Sprintf("%s/.env", home))
+	return err
 }
 
 func initThing() (*device.Thing, error) {
-  thingName := device.ThingName(os.Getenv("THING_NAME"))
-  endpoint := os.Getenv("ENDPOINT")
-  keyPair := device.KeyPair{
-   PrivateKeyPath: os.Getenv("PRIVATE_KEY_PATH"),
-   CertificatePath: os.Getenv("CERT_PATH"),
-   CACertificatePath: os.Getenv("ROOT_CA_PATH"),
-  }
+	thingName := device.ThingName(os.Getenv("THING_NAME"))
+	endpoint := os.Getenv("ENDPOINT")
+	keyPair := device.KeyPair{
+		PrivateKeyPath:    os.Getenv("PRIVATE_KEY_PATH"),
+		CertificatePath:   os.Getenv("CERT_PATH"),
+		CACertificatePath: os.Getenv("ROOT_CA_PATH"),
+	}
 
-  return device.NewThing(keyPair, endpoint, thingName)
+	return device.NewThing(keyPair, endpoint, thingName)
 }
 
 func readTemp() int {
-  // Make sure periph is initialized.
-  if _, err := host.Init(); err != nil {
-      log.Fatal(err)
-  }
+	// Make sure periph is initialized.
+	if _, err := host.Init(); err != nil {
+		log.Fatal(err)
+	}
 
-  // Open default I²C bus.
-  bus, err := i2creg.Open("")
-  if err != nil {
-      log.Fatalf("failed to open I²C: %v", err)
-  }
-  defer bus.Close()
+	// Open default I²C bus.
+	bus, err := i2creg.Open("")
+	if err != nil {
+		log.Fatalf("failed to open I²C: %v", err)
+	}
+	defer bus.Close()
 
-  // Create a new temperature sensor.
-  sensor, err := mcp9808.New(bus, &mcp9808.DefaultOpts)
-  if err != nil {
-      log.Fatalln(err)
-  }
+	// Create a new temperature sensor.
+	sensor, err := mcp9808.New(bus, &mcp9808.DefaultOpts)
+	if err != nil {
+		log.Fatalln(err)
+	}
 
-  // Read values from sensor.
-  measurement, err := sensor.SenseTemp()
+	// Read values from sensor.
+	measurement, err := sensor.SenseTemp()
 
-  if err != nil {
-      log.Fatalln(err)
-  }
+	if err != nil {
+		log.Fatalln(err)
+	}
 
-  return int(measurement.Fahrenheit())
+	return int(measurement.Fahrenheit())
 }
 
 func main() {
-  err := initEnv()
-  if err != nil {
-    panic(err)
-  }
+	err := initEnv()
+	if err != nil {
+		panic(err)
+	}
 
-  initLog()
+	logPath := os.Getenv("LOG_FILE_PATH")
+	f, err := os.OpenFile(logPath, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
+	}
+	defer f.Close()
 
-  // Initilize a new Thing
-  thing, err := initThing()
-  if err != nil {
-    panic(err)
-  }
+	mw := io.MultiWriter(os.Stdout, f)
+	log.SetOutput(mw)
 
-  // Subscribe to shadow
-  shadowChan, err := thing.SubscribeForThingShadowChanges()
-  if err != nil {
-    panic(err)
-  }
+	// Initilize a new Thing
+	thing, err := initThing()
+	if err != nil {
+		panic(err)
+	}
 
-  // Read temperature from sensor
-  data := readTemp()
-  shadow := fmt.Sprintf(`{"state": {"reported": {"temp": %d}}}`, data)
+	// Subscribe to shadow
+	shadowChan, err := thing.SubscribeForThingShadowChanges()
+	if err != nil {
+		panic(err)
+	}
 
-  // Update thing shadow
-  err = thing.UpdateThingShadow(device.Shadow(shadow))
-  if err != nil {
-    panic(err)
-  }
+	// Read temperature from sensor
+	data := readTemp()
+	shadow := fmt.Sprintf(`{"state": {"reported": {"temp": %d}}}`, data)
 
-  updatedShadow, ok := <-shadowChan
-  if !ok {
-    panic("Failed to read from shadow channel")
-  }
+	// Update thing shadow
+	err = thing.UpdateThingShadow(device.Shadow(shadow))
+	if err != nil {
+		panic(err)
+	}
 
-  unmarshaledUpdatedShadow := &Shadow{}
+	updatedShadow, ok := <-shadowChan
+	if !ok {
+		panic("Failed to read from shadow channel")
+	}
 
-  err = json.Unmarshal(updatedShadow, unmarshaledUpdatedShadow)
-  if err != nil {
-    panic(err)
-  }
+	unmarshaledUpdatedShadow := &Shadow{}
+
+	err = json.Unmarshal(updatedShadow, unmarshaledUpdatedShadow)
+	if err != nil {
+		panic(err)
+	}
 
 	log.Printf("Temp: %v Version: %v", unmarshaledUpdatedShadow.State.Reported.Temp, unmarshaledUpdatedShadow.Version)
 }
-
